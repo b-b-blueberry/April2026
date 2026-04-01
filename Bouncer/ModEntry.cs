1ee6978179df033741f60f15677f31a3dbf42682
blueberry <dearbblueberry@gmail.com>
2026-04-01 19:34:46 +1100

    initial commit

---
+++ Bouncer/ModEntry.cs
@@ -0,0 +1,13 @@
+﻿namespace Bouncer;
+
+public class ModEntry : Mod
+{
+    public override void Entry(IModHelper helper)
+    {
+        helper.Events.GameLoop.UpdateTicked += (sender, e) =>
+        {
+            if (e.IsMultipleOf(8) && Game1.getCharacterFromName("Bouncer") is NPC bouncer && bouncer.yJumpOffset == 0)
+                bouncer.jump();
+        };
+    }
+}
